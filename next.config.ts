23dea49c4cb0c74e9120366cbb72e8a335d31523
ChenChengfeng <47024948+chenfeng559@users.noreply.github.com>
2026-02-26 00:26:31 +0800

    Update next.config.ts for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: process.env.NODE_ENV === "production" ? "/carlos_personal_web" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/carlos_personal_web" : "",
   images: {
     unoptimized: true,
   },
